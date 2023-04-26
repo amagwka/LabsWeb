e00a4838d4382f55e188337efc54147436535c69
Mukhamejan <amagwka@gmail.com>
2023-04-26 21:17:14 +0600

    ЛПР 25

--- inc/cookie.inc.php
+++ inc/cookie.inc.php
@@ -1,1 +1,15 @@
-<?
+<?php
+$visitCounter = 0;
+if (isset($_COOKIE["visitCounter"])) {
+    $visitCounter = (int)$_COOKIE["visitCounter"];
+}
+$visitCounter++;
+$lastVisit = "";
+if (isset($_COOKIE["lastVisit"])) {
+    $lastVisit = date("d-m-Y H:i:s", $_COOKIE["lastVisit"]);
+}
+if (date("d-m-Y", $_COOKIE["lastVisit"]) != date("d-m-Y")) {
+    setcookie("visitCounter", $visitCounter, time() + 3600 * 24 * 365);
+    setcookie("lastVisit", time(), time() + 3600 * 24 * 365);
+}
+?>
