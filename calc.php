f7a3b057273ed07d281f85ffef39b793b6ce1a42
Mukhamejan <amagwka@gmail.com>
2023-04-26 20:44:37 +0600

    ЛПР 24

--- calc.php
+++ calc.php
@@ -1,18 +1,61 @@
-    <!-- Область основного контента -->
-    <form action=''>
-      <label>Число 1:</label>
-      <br />
-      <input name='num1' type='text' />
-      <br />
-      <label>Оператор: </label>
-      <br />
-      <input name='operator' type='text' />
-      <br />
-      <label>Число 2: </label>
-      <br />
-      <input name='num2' type='text' />
-      <br />
-      <br />
-      <input type='submit' value='Считать'>
-    </form>
-    <!-- Область основного контента -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $number1 = isset($_POST["num1"]) ? (int) $_POST["num1"] : null;
+  $number2 = isset($_POST["num2"]) ? (int) $_POST["num2"] : null;
+  $operator = isset($_POST["operator"]) ? $_POST["operator"] : null;
+
+  $result = null;
+  $error = null;
+
+  switch ($operator) {
+    case "+":
+      $result = $number1 + $number2;
+      break;
+    case "-":
+      $result = $number1 - $number2;
+      break;
+    case "*":
+      $result = $number1 * $number2;
+      break;
+    case "/":
+      if ($number2 === 0) {
+        $error = "На ноль делить нельзя!";
+      } else {
+        $result = $number1 / $number2;
+      }
+      break;
+    default:
+      $error = "Неверный оператор!";
+  }
+
+  if ($error) {
+    echo "<p style='color:red;'>{$error}</p>";
+  } else {
+    echo "<p>Результат: {$number1} {$operator} {$number2} = {$result}</p>";
+  }
+}
+?>
+<!-- Область основного контента -->
+
+<form action="" method="POST">
+  <label>Число 1:</label>
+  <br />
+  <input name="num1" type="text" />
+  <br />
+  <label>Оператор: </label>
+  <br />
+  <select name="operator">
+    <option value="+">+</option>
+    <option value="-">-</option>
+    <option value="*">*</option>
+    <option value="/">/</option>
+  </select>
+  <br />
+  <label>Число 2: </label>
+  <br />
+  <input name="num2" type="text" />
+  <br />
+  <br />
+  <input type="submit" value="Считать">
+</form>
+<!-- Область основного контента -->
