76d24d9daa835db91e3bb7877cbd3dd756a6bf92
Mukhamejan <amagwka@gmail.com>
2023-05-02 17:37:43 +0600

    переписанный на Vue.js

--- apple/index.php
+++ apple/index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="style.css" />
+  <script src="https://unpkg.com/vue@next"></script>
   <script defer src="main.js"></script>
   <title>Apple</title>
 </head>
@@ -36,56 +37,8 @@
       <a href="">Shop online </a> and fet free, no-contact delivery, Specialist
       help, and more.
     </div>
-    <section class="section1">
-      <div
-        class="background"
-        style="background-image: url(./img/iPhone\ 14.jpg)"
-      ></div>
-
-      <div class="top__title">
-        <h1>iPhone 14 Pro</h1>
-        <h3>It's a great year.</h3>
-        <div class="top__title--links">
-          <a href=""
-            >Learn more <img src="./svg/right-arrow-blue.svg" alt=""
-          /></a>
-          <a href="">Shop <img src="./svg/right-arrow-blue.svg" alt="" /></a>
-        </div>
-      </div>
-
-      <div class="bottom__title">
-        <h2>iPhone 14 pro Max</h2>
-        <h2>Pre-order starting at</h2>
-        <h2>5:00 a.m.PST on 11.6</h2>
-        <h2>Available 11.13</h2>
-      </div>
-    </section>
-    <section class="section2">
-      <div
-        class="background"
-        style="background-image: url(./img/s-2.jpg)"
-      ></div>
-    
-      <div class="top__title">
-        <h1 class="black">iPhone 14</h1>
-        <h3 class="black">Blast past fast</h3>
-        <div class="top__title--links">
-          <a href="" class="dark-blue"
-            >Learn more <img src="./svg/right-arrow-darkBlue.svg" alt=""
-          /></a>
-          <a href="" class="dark-blue"
-            >Shop <img src="./svg/right-arrow-darkBlue.svg" alt=""
-          /></a>
-        </div>
-      </div>
-    
-      <div class="bottom__title">
-        <h2 class="black">iPhone 14 mini</h2>
-        <h2>Pre-order starting at</h2>
-        <h2>5:00 a.m.PST on 11.6</h2>
-        <h2>Available 11.13</h2>
-      </div>
-    </section>
+    <section class="section1" v-html="displayProduct(products[0])"></section>
+    <section class="section2" v-html="displayProduct(products[1])"></section>
 </body>
 
 </html>
