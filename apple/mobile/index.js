d7657a4121007bfa8fdda81dbab8c82ba5e780fd
Mukhamejan <amagwka@gmail.com>
2023-05-02 18:48:59 +0600

    Связать работу вашей продвинутой анимации с таймер

--- apple/mobile/index.js
+++ apple/mobile/index.js
@@ -5,4 +5,16 @@
     burger.onclick = function() {
         header.classList.toggle('menu-opened');
     }
+
+    var colors = ['#ff6666', '#ffb266', '#ffff66', '#b2ff66', '#66ffb2', '#66b2ff', '#b266ff', '#ff66b2'];
+    var colorIndex = 0;
+    setInterval(function() {
+        header.style.backgroundColor = colors[colorIndex];
+        colorIndex++;
+        if (colorIndex == colors.length) {
+            colorIndex = 0;
+        }
+        header.classList.toggle('menu-opened');
+    }, 5000);
+
 }());
